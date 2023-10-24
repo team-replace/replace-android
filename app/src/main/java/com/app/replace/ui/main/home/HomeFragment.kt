95a4d2c603f745c348ff0a5492718598167febaf
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-10-24 17:11:03 +0900

    feat: 홈 프래그먼트 뷰 구현
    (cherry picked from commit 65b44dc15d3eba56509a628b836312954ab3f94e)

--- app/src/main/java/com/app/replace/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/app/replace/ui/main/home/HomeFragment.kt
@@ -6,21 +6,41 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.app.replace.R
+import com.app.replace.databinding.FragmentHomeBinding
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+    private val binding: FragmentHomeBinding by lazy {
+        FragmentHomeBinding.inflate(layoutInflater)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setNaverMap()
+    }
+
+    private fun setNaverMap() {
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.fcv_map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.fcv_map, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
     }
 }
