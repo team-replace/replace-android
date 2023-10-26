8a1dfd9d1082810230fb7ed79b523f4e6e94d0f6
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-10-26 15:11:23 +0900

    feat: 글 작성/수정 페이지 뷰 구현
    (cherry picked from commit 488adeb6cadb4160d1f6ba69323cef7a5776d737)

---
+++ app/src/main/java/com/app/replace/ui/diaryeditor/DiaryEditorActivity.kt
@@ -0,0 +1,12 @@
+package com.app.replace.ui.diaryeditor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.replace.R
+
+class DiaryEditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_editor)
+    }
+}
