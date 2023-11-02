33d1649603c0594cb13a2d41b4fa63e392e7f766
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-11-02 20:58:39 +0900

    refactor: Success 될 때 헤더 값도 같이 넘기도록 수정

--- data/src/main/java/com/replace/data/remote/CustomResult.kt
+++ data/src/main/java/com/replace/data/remote/CustomResult.kt
@@ -2,10 +2,11 @@ package com.replace.data.remote
 
 import com.replace.data.common.CustomThrowable
 import com.replace.data.common.FetchState
+import okhttp3.Headers
 
 sealed class CustomResult<T> {
 
-    class Success<T>(val data: T) : CustomResult<T>()
+    class Success<T>(val data: T, val headers: Headers) : CustomResult<T>()
     class ApiError<T>(val customThrowable: CustomThrowable) : CustomResult<T>()
     class UnKnownApiError<T>(val code: Int) : CustomResult<T>()
     class NetworkError<T>(val fetchState: FetchState) : CustomResult<T>()
