5da19ed9ac6f954d70caf1fff1592a72b25c2e13
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-11-06 22:52:20 +0900

    feat: 로딩 다이얼로그 구현

---
+++ app/src/main/java/com/app/replace/ui/common/dialog/LoadingDialog.kt
@@ -0,0 +1,35 @@
+package com.app.replace.ui.common.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.app.replace.R
+import com.app.replace.databinding.DialogLoadingBinding
+import com.bumptech.glide.Glide
+
+class LoadingDialog(
+    private val message: String,
+) : DialogFragment() {
+
+    private lateinit var binding: DialogLoadingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DialogLoadingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        isCancelable = false
+        Glide.with(this).asGif().load(R.raw.loading).fitCenter().into(binding.ivLoading)
+        binding.tvLoading.text = message
+    }
+}
