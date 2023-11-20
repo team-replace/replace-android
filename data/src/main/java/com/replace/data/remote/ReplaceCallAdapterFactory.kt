7560634c063491251c994d4e853cbd70990853f0
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-11-20 17:15:46 +0900

    feat: 서버 연결

--- data/src/main/java/com/replace/data/remote/ReplaceCallAdapterFactory.kt
+++ data/src/main/java/com/replace/data/remote/ReplaceCallAdapterFactory.kt
@@ -12,12 +12,17 @@ class ReplaceCallAdapterFactory : CallAdapter.Factory() {
         annotations: Array<out Annotation>,
         retrofit: Retrofit,
     ): CallAdapter<*, *>? {
-        if (Call::class.java != getRawType(returnType)) return null
-        check(returnType is ParameterizedType)
+        if (getRawType(returnType) != Call::class.java) {
+            return null
+        }
+        check(returnType is ParameterizedType) { "Return type must be parameterized as Call<CustomResult<T>>" }
 
         val responseType = getParameterUpperBound(0, returnType)
-        if (getRawType(responseType) != CustomResult::class.java) return null
-        check(responseType is ParameterizedType)
+        if (getRawType(responseType) != CustomResult::class.java) {
+            return null
+        }
+
+        check(responseType is ParameterizedType) { "Response type must be parameterized as CustomResult<T>" }
 
         val successType = getParameterUpperBound(0, responseType)
         return ReplaceCallAdapter<Any>(successType)
